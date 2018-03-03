94b4abbdf5ce7fe6c5e1b566b70117817a335dbd
Tom Cash <tom@substrakt.com>
2018-03-03 15:44:32 +0000

    UPDATE: Tidying new Sass.

--- single.php
+++ single.php
@@ -1,17 +1,17 @@
 <?php get_header(); ?>
 
-<section class="single-info">
+<section class="info">
     <h1><?php echo $julez->title(); ?></h1>
     <h6>~ <?php echo $julez->date('F jS, Y'); ?> ~</h6>
 </section>
 
 <?php if ($introduction = $julez->introduction()): ?>
-    <section class="single-introduction">
+    <section class="introduction">
         <?php echo $introduction; ?>
     </section>
 <?php endif; ?>
 
-<section class="single-content">
+<section class="content">
     <?php construkt(); ?>
 </section>
 
